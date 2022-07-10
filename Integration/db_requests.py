c05500d5ed080d1951a14e6d87fb4d2365cc9ca0
AleksandrPetrov89 <petrovav89@gmail.com>
2022-07-10 12:52:09 +0500

    installed stubs "pass" in Integration/db_requests.py

--- Integration/db_requests.py
+++ Integration/db_requests.py
@@ -1,11 +1,12 @@
 def check_blacklist(user_id):
     # Вызов функции на получение ЧС
-    blacklist =
+    # blacklist =
+    pass
+
 
 def send_bot_user_data(bot_user_data):
     pass
 
+
 def send_photo_like_id(photo_id):
     pass
-
-def
