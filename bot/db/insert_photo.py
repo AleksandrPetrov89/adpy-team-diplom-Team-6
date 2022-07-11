2b5eb2afcf2e4220941bf52896a37be1902ad7d2
Олег Матшин <89175955650@mail.ru>
2022-07-11 21:16:24 +0300

    Some modified code in methods delete_photo and insert_photo.

--- bot/db/insert_photo.py
+++ bot/db/insert_photo.py
@@ -1,4 +1,4 @@
-from db.create_table import TableDb
+from create_table import TableDb
 
 
 #
@@ -47,31 +47,41 @@ class Photo:
     # Функция заносит данные по фотографиям в таблицу photo_list.
     def in_photolist_table(self):
         """
-        Функция внесения в БД данных о фотографиях: ссылка, ID фотографии, ID пользователя.
-        :return: insert_photo_result
+        Функция внесения в БД данных о фотографиях: ссылка, ID фотографии, ID пользователя. Если такие данные уже есть
+         в таблице, то ничего не заносится. Возвращается результат выполнения insert_status. Если запись сделана, то
+         True, если нет, то False.
+        :return: insert_status
         """
         dict_obj = Photo(self.data_base, self.user)
         dict_photo = dict_obj.search_link_symbol()
         table_db_obj = TableDb(self.data_base, self.user)
         connect = table_db_obj.db_connect()
+        insert_status = True
         for key, value in dict_photo.items():
             req_sql = f"INSERT INTO photo_list(photo_link, photo_id, user_data_user_id) VALUES('{value[1]}'," \
                       f" {value[2]}, {value[0]});"
-            insert_photo_result = connect.execute(req_sql)
-        return insert_photo_result
+            req_check_sql = f'SELECT user_data_user_id, photo_id FROM photo_list' \
+                               f' WHERE user_data_user_id={value[0]} AND photo_id={value[2]};'
+            is_exist = connect.execute(req_check_sql).fetchall()
+            if is_exist != []:
+                insert_status = False
+            else:
+                connect.execute(req_sql)
+        return insert_status
 
     # Функция заносит данные по "лайкнутым" фотографиям в таблицу likes_list.
     def in_likeslist_table(self):
         """
         Функция внесения в БД данных о фотографиях, на которые поставили "лайк": ID пользователя, который поставил лайк,
         а также ID фотографии, которая понравилась.
-        :return: insert_likes_photo_result
+        :return: insert_status
         """
         id_list = []
         dict_obj = Photo(self.data_base, self.user)
         dict_photo = dict_obj.get_photo()
         TableDb_obj = TableDb(self.data_base, self.user)
         connect = TableDb_obj.db_connect()
+        insert_status = True
         for key, value in dict_photo.items():
             req_search_id = f'SELECT id FROM photo_list WHERE photo_id={value[2]};'
             list_photo_list_id = connect.execute(req_search_id).fetchall()
@@ -79,10 +89,10 @@ class Photo:
                 for id_value in item:
                     id_list.append(id_value)
                     req_sql = f"INSERT INTO likes_list(bot_user_user_id, photo_list_id) VALUES({key}, {id_value});"
-                    insert_likes_photo_result = connect.execute(req_sql)
-        return insert_likes_photo_result
+                    connect.execute(req_sql)
+        return insert_status
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # Photo.search_link_symbol(Photo('db_dating', 'user_dating'))
     # Photo.in_photolist_table(Photo('db_dating', 'user_dating'))
-    # Photo.in_likeslist_table(Photo('db_dating', 'user_dating'))
+    Photo.in_likeslist_table(Photo('db_dating', 'user_dating'))
