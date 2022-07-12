380ff3ee1730829ac8ef7270993b081604c7333c
Олег Матшин <89175955650@mail.ru>
2022-07-12 19:34:58 +0300

    New function del_null_user for cleaning null user data.

--- bot/db/delete_data.py
+++ bot/db/delete_data.py
@@ -64,7 +64,52 @@ class Disposal:
         after_del_list = list(set(after_del_list))
         return after_del_list
 
+    # Функция удаления "пустых" несвязанных данных пользователей из таблицы user_data по id пользователя.
+    def del_null_user(self, user=123456780):
+        """
+        Функция осуществляет проверку и чистку в таблице user_data базы данных. Если в таблице в поле есть пользователь
+        user_id со всеми другими пустыми полями и при условии, что user_id отсутствует во всех остальных связанных
+        таблицах, то эта запись удаляется из таблицы, и, соответственно, из базы. Начальный статус операции: False.
+        Если запись удалена, то статус меняется на True. После успешного удаления записи также возвращается пустой
+        список check_id_list.
+        :return: clean_status, check_id_list
+        """
+        clean_status = False
+        main_check_list = []
+        table_db_obj = TableDb(self.data_base, self.user)
+        connect = table_db_obj.db_connect()
+        req_id_check = f'SELECT user_id FROM user_data WHERE user_id={user};'
+        check_list_1 = connect.execute(req_id_check).fetchall()
+        req_id_tables_check = f'SELECT bot_user_user_id FROM likes_list WHERE bot_user_user_id={user};'
+        check_list_2 = connect.execute(req_id_tables_check).fetchall()
+        if check_list_2 != []:
+            main_check_list.append(check_list_2)
+        req_id_el_check = f'SELECT user_data_user_id, bot_user_user_id FROM elected_list WHERE ' \
+                          f'user_data_user_id={user} or bot_user_user_id={user};'
+        check_list_3 = connect.execute(req_id_el_check).fetchall()
+        if check_list_3 != []:
+            main_check_list.append(check_list_3)
+        req_id_bl_check = f'SELECT user_data_user_id, bot_user_user_id FROM black_list WHERE ' \
+                          f'user_data_user_id={user} or bot_user_user_id={user};'
+        check_list_4 = connect.execute(req_id_bl_check).fetchall()
+        if check_list_4 != []:
+            main_check_list.append(check_list_4)
+        req_id_pl_check = f'SELECT user_data_user_id FROM photo_list WHERE ' \
+                          f'user_data_user_id={user};'
+        check_list_5 = connect.execute(req_id_pl_check).fetchall()
+        if check_list_5 != []:
+            main_check_list.append(check_list_5)
+        if check_list_1 != []:
+            if main_check_list == []:
+                req_del_user_id = f'DELETE FROM user_data WHERE user_id={user};'
+                connect.execute(req_del_user_id)
+                clean_status = True
+        req_del_check = f'SELECT user_id FROM user_data WHERE user_id={user};'
+        check_id_list = connect.execute(req_del_check).fetchall()
+        return clean_status, check_id_list
 
-# if __name__ == '__main__':
+
+if __name__ == '__main__':
     # Disposal.del_id_blacklist(Disposal('db_dating', 'user_dating'))
     # Disposal.del_id_electlist(Disposal('db_dating', 'user_dating'))
+    Disposal.del_null_user(Disposal('db_dating', 'user_dating'))
